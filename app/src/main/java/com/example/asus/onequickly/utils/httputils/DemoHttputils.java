03bb5cfbb7373ffed83d500033acb06ba831734d
lanbo9592 <835301149@qq.com>
2018-01-25 10:57:51 +0800

    提供帮助类

--- app/src/main/java/com/example/asus/onequickly/utils/httputils/DemoHttputils.java
+++
@@ -1,8 +0,0 @@
-package com.example.asus.onequickly.utils.httputils;
-
-/**
- * Created by asus on 2018/1/25.
- */
-
-public class DemoHttputils {
-}
---
+++ app/src/main/java/com/example/asus/onequickly/view/customview/MyToolBar.java
@@ -0,0 +1,29 @@
+package com.example.asus.onequickly.view.customview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by asus on 2018/1/25.
+ */
+
+public class MyToolBar extends RelativeLayout {
+    public MyToolBar(Context context) {
+        this(context,null);
+    }
+
+    public MyToolBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+
+    }
+
+
+
+
+    public MyToolBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
