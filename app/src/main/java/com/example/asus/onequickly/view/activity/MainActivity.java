5f9f9ee04ecc14d6c3ace060969aa5b6c170bea3
lanbo9592 <835301149@qq.com>
2018-01-25 18:42:49 +0800

    //添加了侧拉栏

--- app/src/main/java/com/example/asus/onequickly/view/activity/MainActivity.java
+++ app/src/main/java/com/example/asus/onequickly/view/activity/MainActivity.java
@@ -29,11 +29,57 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
         MyToolBar main_tool_bar=findViewById(R.id.main_tool_bar);
         //设置标题的方法
-        main_tool_bar.settittle("推荐");
+        main_tool_bar.setTittle("推荐");
 
+        //////////////////////////////////////NavigationView导航菜单////////////////////////////////////////////////
+        mViewNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
 
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                item.setChecked(false);
+
+                switch (item.getItemId()) {
+                    case R.id.action_concern:
+                        Toast.makeText(MainActivity.this, "我的关心", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_collect:
+                        Toast.makeText(MainActivity.this, "我的收藏", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_Search:
+                        Toast.makeText(MainActivity.this, "搜索好友", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_inform:
+                        Toast.makeText(MainActivity.this, "消息通知", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.ic_menu_night:
+                        Toast.makeText(MainActivity.this, "夜间模式", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.ic_menu_my_works:
+                        Toast.makeText(MainActivity.this, "我的作品", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.ic_menu_set:
+                        Toast.makeText(MainActivity.this, "设置", Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
+
+                //关闭侧拉
+                mDrawerLayout.closeDrawers();
+                return false;
+            }
+        });
+
+        mainToolBar.setOnLeftButtonClickListener(new MyToolBar.OnLeftButtonClickListener() {
+            @Override
+            public void onClick() {
+                mDrawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
 
     }
 }
