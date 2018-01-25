c16280d82aea17f5a7e2ad677efc102c763817e1
zhangrui19941025 <1833175648@qq.com>
2018-01-25 17:09:53 +0800

    Application mainActivity toolBar res

--- app/src/main/java/com/example/asus/onequickly/view/activity/MainActivity.java
+++ app/src/main/java/com/example/asus/onequickly/view/activity/MainActivity.java
@@ -1,21 +1,83 @@
 package com.example.asus.onequickly.view.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.asus.onequickly.R;
 import com.example.asus.onequickly.view.customview.MyToolBar;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.main_tool_bar)
+    MyToolBar mainToolBar;
+    @BindView(R.id.view_navigation)
+    NavigationView mViewNavigationView;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        MyToolBar main_tool_bar=findViewById(R.id.main_tool_bar);
+        ButterKnife.bind(this);
         //设置标题的方法
-        main_tool_bar.settittle("推荐");
+        mainToolBar.setTittle("推荐");
 
+
+        //////////////////////////////////////NavigationView导航菜单////////////////////////////////////////////////
+        mViewNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                item.setChecked(false);
+
+                switch (item.getItemId()) {
+                    case R.id.action_concern:
+                        Toast.makeText(MainActivity.this, "我的关心", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_collect:
+                        Toast.makeText(MainActivity.this, "我的收藏", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_Search:
+                        Toast.makeText(MainActivity.this, "搜索好友", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_inform:
+                        Toast.makeText(MainActivity.this, "消息通知", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.ic_menu_night:
+                        Toast.makeText(MainActivity.this, "夜间模式", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.ic_menu_my_works:
+                        Toast.makeText(MainActivity.this, "我的作品", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.ic_menu_set:
+                        Toast.makeText(MainActivity.this, "设置", Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
+
+                //关闭侧拉
+                mDrawerLayout.closeDrawers();
+                return false;
+            }
+        });
+
+        mainToolBar.setOnLeftButtonClickListener(new MyToolBar.OnLeftButtonClickListener() {
+            @Override
+            public void onClick() {
+                mDrawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
     }
 }
