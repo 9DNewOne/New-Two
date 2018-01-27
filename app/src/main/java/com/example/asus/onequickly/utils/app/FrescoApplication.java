fa57688401ad791886fd8bed10688ee3a99ae430
gq <1057386657@qq.com>
2018-01-27 11:29:32 +0800

    QQ登录

--- app/src/main/java/com/example/asus/onequickly/utils/app/FrescoApplication.java
+++ app/src/main/java/com/example/asus/onequickly/utils/app/FrescoApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.Context;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.umeng.socialize.PlatformConfig;
+import com.umeng.socialize.UMShareAPI;
 
 /**
  * Created by asus on 2018/1/25.
@@ -11,13 +13,20 @@ import com.facebook.drawee.backends.pipeline.Fresco;
 
 public class FrescoApplication extends Application {
 
-    private  static Context context;
+
+
+    private static Context context;//获取全局上下文
+    {
+        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
+
+    }
 
     @Override
     public void onCreate() {
         super.onCreate();
         //初始化fresco
         Fresco.initialize(this);
+        UMShareAPI.get(this);
 
         //设置全局上下文
         context=getApplicationContext();
--- app/src/main/java/com/example/asus/onequickly/view/activity/SecondActivity.java
+++ app/src/main/java/com/example/asus/onequickly/view/activity/SecondActivity.java
@@ -3,6 +3,7 @@ package com.example.asus.onequickly.view.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -11,15 +12,23 @@ import android.widget.Toast;
 
 
 import com.example.asus.onequickly.R;
+import com.umeng.socialize.UMAuthListener;
+import com.umeng.socialize.UMShareAPI;
+import com.umeng.socialize.bean.SHARE_MEDIA;
 
+import java.util.Map;
 
   /*
   *  做登录操作的activity
   * */
 
+//  /*
+//   第三方需要5个jar包 在libs 里面 还需要Application里初始化
+//
+// */
 public class SecondActivity extends AppCompatActivity {
           //API集成
-   // private UMAuthListener authListener;
+    private UMAuthListener authListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,69 +50,68 @@ public class SecondActivity extends AppCompatActivity {
           qqlonginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-             //UMShareAPI.get(SecondActivity.this).getPlatformInfo(SecondActivity.this, SHARE_MEDIA.QQ, authListener );
-              }
+
+             UMShareAPI.get(SecondActivity.this).getPlatformInfo(SecondActivity.this, SHARE_MEDIA.QQ, authListener );
+
+
+            }
         });
-
-
 
            //其他方式登录
        TextView  textviewqita=findViewById(R.id.textviewqita);
         textviewqita.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                startActivity(new Intent(SecondActivity.this,OtherloginActivity.class));
-
             }
         });
 
-
-
-        //   authListener = new UMAuthListener() {
+          authListener = new UMAuthListener() {
                 /**
                  * @desc 授权开始的回调
                  * @param platform 平台名称
                  */
                 //@Override
-             /*   public void onStart(SHARE_MEDIA platform) {
-                }
-    */
+               public void onStart(SHARE_MEDIA platform) {
+
+               }
+
+
                 /**
                  * @desc 授权成功的回调
                  * @param platform 平台名称
                  * @param action 行为序号，开发者用不上
                  * @param data 用户资料返回
                  */
-              /*  @Override
+
+                    @Override
                 public void onComplete(SHARE_MEDIA platform, int action, Map<String, String> data) {
 
                     Toast.makeText(SecondActivity.this, "成功了", Toast.LENGTH_LONG).show();
+                        Log.i("-------",data.toString());
 
-                }*/
-
+                }
                 /**
                  * @desc 授权失败的回调
                  * @param platform 平台名称
                  * @param action 行为序号，开发者用不上
                  * @param t 错误原因
                  */
-              /*  @Override
+                @Override
                 public void onError(SHARE_MEDIA platform, int action, Throwable t) {
                     Toast.makeText(SecondActivity.this, "失败：" + t.getMessage(),                                  Toast.LENGTH_LONG).show();
-                }*/
+                }
 
                 /**
                  * @desc 授权取消的回调
                  * @param platform 平台名称
                  * @param action 行为序号，开发者用不上
                  */
-              /*  @Override
+                @Override
                 public void onCancel(SHARE_MEDIA platform, int action) {
                     Toast.makeText(SecondActivity.this, "取消了", Toast.LENGTH_LONG).show();
                 }
-            };*/
-
+            };
 
           //箭头图片返回上一页
         ImageView imaeviewback=findViewById(R.id.imageback);
@@ -115,10 +123,11 @@ public class SecondActivity extends AppCompatActivity {
            });
     }
 
-   /* @Override
+
+   @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         UMShareAPI.get(this).onActivityResult(requestCode, resultCode, data);
-    }*/
+    }
 
 }
