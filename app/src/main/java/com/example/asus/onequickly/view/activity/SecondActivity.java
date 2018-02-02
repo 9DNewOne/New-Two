294afb6fc2c77ffabc9e4bd8ef53404dc282ad89
gq <1057386657@qq.com>
2018-02-02 20:20:16 +0800

    2018 0202 20:18   提供有效第三方登录

--- app/src/main/java/com/example/asus/onequickly/view/activity/SecondActivity.java
+++ app/src/main/java/com/example/asus/onequickly/view/activity/SecondActivity.java
@@ -9,16 +9,13 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.example.asus.onequickly.R;
 import com.umeng.socialize.UMAuthListener;
 import com.umeng.socialize.UMShareAPI;
 import com.umeng.socialize.bean.SHARE_MEDIA;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.util.Map;
+
   /*
   *  做登录操作的activity
   * */
@@ -28,6 +25,7 @@ import java.util.Map;
 //
 // */
 public class SecondActivity extends AppCompatActivity {
+  private UMAuthListener authListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +35,8 @@ public class SecondActivity extends AppCompatActivity {
         BackpageUp();//箭头图片返回上一页
         weixinlog();  //微信第三方登录
         otherlog();//其他方式登录
+
+
         //QQ第三方登录
         Button qqlonginbtn = findViewById(R.id.qqlonginbtn);
         qqlonginbtn.setOnClickListener(new View.OnClickListener() {
@@ -46,57 +46,70 @@ public class SecondActivity extends AppCompatActivity {
             }
         });
 
+
+         authListener = new UMAuthListener() {
+            /**
+             * @desc 授权开始的回调
+             * @param platform 平台名称
+             */
+            @Override
+            public void onStart(SHARE_MEDIA platform) {
+
+            }
+
+            /**
+             * @desc 授权成功的回调
+             * @param platform 平台名称
+             * @param action 行为序号，开发者用不上
+             * @param data 用户资料返回
+             */
+            @Override
+            public void onComplete(SHARE_MEDIA platform, int action, Map<String, String> data) {
+
+                Toast.makeText(SecondActivity.this, "成功了", Toast.LENGTH_LONG).show();
+
+                 String iconurl = data.get("iconurl");
+                 String  name = data.get("name");
+                 String grander = data.get("gender");
+
+                Toast.makeText(SecondActivity.this, "登录成功"+ name + grander, Toast.LENGTH_LONG).show();
+                Log.i("-----------", iconurl + name + grander);
+                
+
+            }
+
+            /**
+             * @desc 授权失败的回调
+             * @param platform 平台名称
+             * @param action 行为序号，开发者用不上
+             * @param t 错误原因
+             */
+            @Override
+            public void onError(SHARE_MEDIA platform, int action, Throwable t) {
+
+                Toast.makeText(SecondActivity.this, "失败：" + t.getMessage(),                                  Toast.LENGTH_LONG).show();
+            }
+
+            /**
+             * @desc 授权取消的回调
+             * @param platform 平台名称
+             * @param action 行为序号，开发者用不上
+             */
+            @Override
+            public void onCancel(SHARE_MEDIA platform, int action) {
+                Toast.makeText(SecondActivity.this, "取消了", Toast.LENGTH_LONG).show();
+            }
+        };
+
     }
 
-    UMAuthListener authListener = new UMAuthListener() {
-        /**
-         * @desc 授权开始的回调
-         * @param platform 平台名称
-         */
-        //@Override
-        public void onStart(SHARE_MEDIA platform) {
 
-        }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        UMShareAPI.get(this).onActivityResult(requestCode, resultCode, data);
 
+    }
 
-        /**
-         * @desc 授权成功的回调
-         * @param platform 平台名称
-         * @param action 行为序号，开发者用不上
-         * @param data 用户资料返回
-         */
-
-        @Override
-        public void onComplete(SHARE_MEDIA platform, int action, Map<String, String> data) {
-            String iconurl = data.get("iconurl");
-            String name = data.get("name");
-            String grander = data.get("gender");
-            Toast.makeText(SecondActivity.this, "登录成功" + name + grander, Toast.LENGTH_LONG).show();
-            Log.i("-----------", iconurl + name + grander);
-
-        }
-
-        /**
-         * @desc 授权失败的回调
-         * @param platform 平台名称
-         * @param action 行为序号，开发者用不上
-         * @param t 错误原因
-         */
-        @Override
-        public void onError(SHARE_MEDIA platform, int action, Throwable t) {
-            Toast.makeText(SecondActivity.this, "失败：" + t.getMessage(), Toast.LENGTH_LONG).show();
-        }
-
-        /**
-         * @desc 授权取消的回调
-         * @param platform 平台名称
-         * @param action 行为序号，开发者用不上
-         */
-        @Override
-        public void onCancel(SHARE_MEDIA platform, int action) {
-            Toast.makeText(SecondActivity.this, "取消了", Toast.LENGTH_LONG).show();
-        }
-    };
 
     private void otherlog() {
         //其他方式登录
@@ -111,14 +124,6 @@ public class SecondActivity extends AppCompatActivity {
         });
 
     }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        UMShareAPI.get(this).onActivityResult(requestCode, resultCode, data);
-    }
-
-
     private void weixinlog() {
         //微信第三方登录
         Button weixinbutton = findViewById(R.id.weixinbutton);
@@ -131,7 +136,6 @@ public class SecondActivity extends AppCompatActivity {
         });
 
     }
-
     private void BackpageUp() {
         //箭头图片返回上一页
         ImageView imaeviewback = findViewById(R.id.imageback);
