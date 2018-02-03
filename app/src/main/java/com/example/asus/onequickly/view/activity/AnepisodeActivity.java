da124fa128d1a04109af1fdff0d57c7b2b19f50b
Hxx0910 <2980670658@qq.com>
2018-02-03 09:41:30 +0800

    点击导航栏最右边中的视频和段子。可拍照。

---
+++ app/src/main/java/com/example/asus/onequickly/view/activity/AnepisodeActivity.java
@@ -0,0 +1,57 @@
+package com.example.asus.onequickly.view.activity;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+
+import com.example.asus.onequickly.R;
+
+public class AnepisodeActivity extends AppCompatActivity {
+    private EditText etContent;
+    private View popupWindow;
+    private PopupWindow mPopupWindow;
+    private Handler mHandler=new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anepisode);
+
+        etContent= findViewById(R.id.etContent);
+        popupWindow=getLayoutInflater().inflate(R.layout.layout_popwindow,new LinearLayout(this),false);
+        mPopupWindow = new PopupWindow(popupWindow, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
+        mPopupWindow.setBackgroundDrawable(new BitmapDrawable());
+        mPopupWindow.setTouchable(true);
+        mPopupWindow.setOutsideTouchable(true);
+        etContent.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("WrongConstant")
+            @Override
+            public void onClick(View v) {
+
+                mPopupWindow.setSoftInputMode(PopupWindow.INPUT_METHOD_NEEDED);
+                mPopupWindow.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+
+                mHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
+                    }
+                }, 500);
+                mPopupWindow.showAtLocation(AnepisodeActivity.this.findViewById(R.id.line), Gravity.BOTTOM, 0, 0);
+            }
+        });
+
+    }
+}
